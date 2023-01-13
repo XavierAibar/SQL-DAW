aae5d52a7bc26fdc2538aff998581ef963ede4ae
xam1DAW-M <xavaibmar@alu.edu.gva.es>
2023-01-13 16:44:21 +0100

    Update Ejercicio3.sql

--- Ejercicio3.sql
+++ Ejercicio3.sql
@@ -4,7 +4,7 @@ CREATE TABLE model
  name VARCHAR(16),
  year int NOT NULL,
  patent VARCHAR(16),
- CONSTRAINT pk_model PRIMARY KEY(id)
+ CONSTRAINT pk_model PRIMARY KEY(id),
  CONSTRAINT unique_model UNIQUE(patent)
 );
 INSERT INTO model(id,name,year,patent) VALUES(1, 'Seat C1',  1998, 'C1') 
@@ -18,12 +18,12 @@ CREATE TABLE cars
 (
   id BIGINT,
   plate VARCHAR(16),
-  color VARCHAR(16),
+  color VARCHAR(64),
   year INT NOT NULL,
   model BIGINT NOT NULL,
-  CONSTRAINT pk_cars PRIMARY KEY(id)
-  CONSTRAINT unique_cars UNIQUE(plate)
-  CONSTRAINT fk_model FOREIGN KEY(model) REFERENCES model(id)
+  CONSTRAINT pk_cars PRIMARY KEY(id),
+  CONSTRAINT unique_cars UNIQUE(plate),
+  CONSTRAINT fk_model FOREIGN KEY(model) REFERENCES model(id) ON UPDATE cascade ON DELETE cascade
 );
 INSERT INTO cars(id, plate, color, year, model) VALUES (1, 'AA', 'Red', 1997, 1)  
 INSERT INTO cars(id, plate, color, year, model) VALUES (2, 'BB', 'Blue', 1998, 2)
@@ -37,23 +37,52 @@ INSERT INTO cars(id, plate, color, year, model) VALUES (9, 'II', 'Black', 2003, 
 INSERT INTO cars(id, plate, color, year, model) VALUES (10, 'JJ', 'Magenta', 2005, 5)
 INSERT INTO cars(id, plate, color, year, model) VALUES (11, 'KK', 'Gray', 2005, 6)
 INSERT INTO cars(id, plate, color, year, model) VALUES (12, 'LL', 'Purple', 2005, 5)
-INSERT INTO cars(id, plate, color, year, model) VALUES (7, 'GG', 'Yellow', 2006, 6)
-
+INSERT INTO cars(id, plate, color, year, model) VALUES (13, 'MM', 'Yellow', 2006, 6)
+SELECT * FROM model;
 /*Ejercicio 1*/
 SELECT plate, color
 FROM cars;
 /*Ejercicio 2*/
 SELECT patent, name
-FROM cars;
-
+FROM cars AS c INNER JOIN model as m ON model = m.id; 
 /*Ejercicio 3*/
+SELECT c.plate, c.color, m.name, m.patent
+FROM cars AS c INNER JOIN model as m ON model = m.id;
 /*Ejercicio 4*/
+SELECT plate
+FROM cars AS c INNER JOIN model as m ON model = m.id
+WHERE m.year > c.year;
 /*Ejercicio 5*/
+SELECT DISTINCT color 
+FROM cars AS c INNER JOIN model as m ON model = m.id
+WHERE m.year >= 2000
+ORDER BY color DESC;
 /*Ejercicio 6*/
+SELECT c.plate, c.color
+FROM cars AS c INNER JOIN model as m ON model = m.id
+WHERE m.name LIKE '%Citröen%';
 /*Ejercicio 7*/
+
 /*Ejercicio 8*/
+SELECT MAX(c.year)
+FROM cars AS c INNER JOIN model as m ON model = m.id
+WHERE m.name LIKE '%Seat%';
 /*Ejercicio 9*/
+SELECT AVG(year)
+FROM model;
 /*Ejercicio 10*/
-/*Ejercicio 11*/
+SELECT patent
+FROM model
+WHERE patent LIKE '%A%'
+/*Ejercicio 11 NO*/
+SELECT name
+FROM model
+WHERE year > AVG(year);
 /*Ejercicio 12*/
+SELECT m.id
+FROM cars AS c INNER JOIN model as m ON model = m.id
+WHERE c.color LIKE 'red';
 /*Ejercicio 13*/
+SELECT m.name
+FROM cars AS c INNER JOIN model as m ON model = m.id
+WHERE m.id NOT IN (cars);
