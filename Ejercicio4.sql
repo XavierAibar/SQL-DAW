66779e11a076827f501dd55aa136b8f80b0ecf5c
xam1DAW-M <118368998+xam1DAW-M@users.noreply.github.com>
2023-01-24 14:28:52 +0100

    Update Ejercicio4.sql

--- Ejercicio4.sql
+++ Ejercicio4.sql
@@ -5,7 +5,23 @@ CREATE TABLE services
   description TEXT,
   CONSTRAINT pk_services PRIMARY KEY(id)
 );
---Create
+INSERT INTO services (id, name, description) VALUES (1, 'Servicio 1', 'A');
+INSERT INTO services (id, name, description) VALUES (2, 'Servicio 2', 'B');
+INSERT INTO services (id, name, description) VALUES (3, 'Servicio 3', 'C');
+INSERT INTO services (id, name, description) VALUES (4, 'Servicio 4', 'D');
+INSERT INTO services (id, name, description) VALUES (5, 'Servicio 5', 'E');
+INSERT INTO services (id, name, description) VALUES (6, 'Servicio 6', 'F');
+INSERT INTO services (id, name, description) VALUES (7, 'Servicio 7', 'G');
+INSERT INTO services (id, name, description) VALUES (8, 'Servicio 8', 'H');
+INSERT INTO services (id, name, description) VALUES (9, 'Servicio 9', 'I');
+INSERT INTO services (id, name, description) VALUES (10, 'Servicio 10', 'J');
+INSERT INTO services (id, name, description) VALUES (11, 'Servicio 11', 'K');
+INSERT INTO services (id, name, description) VALUES (12, 'Servicio 12', 'L');
+INSERT INTO services (id, name, description) VALUES (13, 'Servicio 13', 'M');
+INSERT INTO services (id, name, description) VALUES (14, 'Servicio 14', 'N');
+INSERT INTO services (id, name, description) VALUES (15, 'Servicio 15', 'O');
+
+
 CREATE TABLE hotels
 (
   id BIGINT,
@@ -13,7 +29,12 @@ CREATE TABLE hotels
   foundation_date DATE,
   CONSTRAINT pk_hotels PRIMARY KEY(id)
 );
---Tabla offers
+INSERT into hotels (id, name, foundation_date) VALUES (1, 'Hotel Jose', '2000-12-12');
+INSERT into hotels (id, name, foundation_date) VALUES (2, 'Hotel Pepe', '2002-01-20');
+INSERT into hotels (id, name, foundation_date) VALUES (3, 'Hotel Barrio', '2003-12-22');
+INSERT into hotels (id, name, foundation_date) VALUES (4, 'Hotel Rojo', '2004-08-30');
+INSERT into hotels (id, name, foundation_date) VALUES (5, 'Hotel Santa Pola', '2005-05-12');
+
 CREATE TABLE offers
 (
   service_id BIGINT,
@@ -22,49 +43,6 @@ CREATE TABLE offers
   CONSTRAINT fk_offers_services FOREIGN KEy(service_id) REFERENCES services(id) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT fk_offers_hotels FOREIGN KEY(hotel_id) REFERENCES hotels(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-INSERT INTO services (id, name, description)
-VALUES (1, 'Servicio1', 'Descripción 1');
-INSERT INTO services (id, name, description)
-VALUES (2, 'Servicio2', 'Descripción 2');
-INSERT INTO services (id, name, description)
-VALUES (3, 'Servicio3', 'Descripción 3');
-INSERT INTO services (id, name, description)
-VALUES (4, 'Servicio4', 'Descripción 4');
-INSERT INTO services (id, name, description)
-VALUES (5, 'Servicio5', 'Descripción 5');
-INSERT INTO services (id, name, description)
-VALUES (6, 'Servicio6', 'Descripción 6');
-INSERT INTO services (id, name, description)
-VALUES (7, 'Servicio7', 'Descripción 7');
-INSERT INTO services (id, name, description)
-VALUES (8, 'Servicio8', 'Descripción 8');
-INSERT INTO services (id, name, description)
-VALUES (9, 'Servicio9', 'Descripción 9');
-INSERT INTO services (id, name, description)
-VALUES (10, 'Servicio10', 'Descripción 10');
-INSERT INTO services (id, name, description)
-VALUES (11, 'Servicio11', 'Descripción 11');
-INSERT INTO services (id, name, description)
-VALUES (12, 'Servicio12', 'Descripción 12');
-INSERT INTO services (id, name, description)
-VALUES (13, 'Servicio13', 'Descripción 13');
-INSERT INTO services (id, name, description)
-VALUES (14, 'Servicio14', 'Descripción 14');
-INSERT INTO services (id, name, description)
-VALUES (15, 'Servicio15', 'Descripción 15');
-SELECT * FROM services;
---Insetar hoteles 
-INSERT into hotels (id, name, foundation_date)
-VALUES (1, 'Hotel1', '2000-10-20');
-INSERT into hotels (id, name, foundation_date)
-VALUES (2, 'Hotel2', '2001-11-11');
-INSERT into hotels (id, name, foundation_date)
-VALUES (3, 'Hotel3', '2002-12-22');
-INSERT into hotels (id, name, foundation_date)
-VALUES (4, 'Hotel4', '2003-08-01');
-INSERT into hotels (id, name, foundation_date)
-VALUES (5, 'Hotel5', '2004-07-02');
-SELECT * FROM hotels; 
 INSERT INTO offers (hotel_id, service_id)
 VALUES (1, 2);
 INSERT INTO offers (hotel_id, service_id)
@@ -111,28 +89,28 @@ INSERT INTO offers (hotel_id, service_id)
 VALUES (4, 12);
 INSERT INTO offers (hotel_id, service_id)
 VALUES (4, 13);
---1
+/*Ejercicio 1*/
 SELECT services.name, description
 FROM services;
---2
+/*Ejercicio 2*/
 SELECT hotels.name, foundation_date
 FROM hotels;
---3
+/*Ejercicio 3*/
 SELECT hotels.name
 FROM services INNER JOIN offers ON services.id = service_id INNER JOIN hotels ON hotel_id = hotels.id
 WHERE foundation_date > '2003';
---4
+/*Ejercicio 4*/
 SELECT hotels.name, services.name, description
 FROM services INNER JOIN offers ON services.id = service_id INNER JOIN hotels ON hotel_id = hotels.id;
---5 NO SALE
+/*Ejercicio 5*/
 SELECT services.name
 FROM services INNER JOIN offers ON services.id = service_id INNER JOIN hotels ON hotel_id = hotels.id
 WHERE foundation_date > '2003-08-2';
---6
+/*Ejercicio 6*/
 SELECT hotels.name, services.name
 FROM services INNER JOIN offers ON services.id = service_id INNER JOIN hotels ON hotel_id = hotels.id
 WHERE hotels.name LIKE '%A%' AND LEN(services.name) > 8;
---7
+/*Ejercicio 7*/
 SELECT hotels.name
 FROM hotels
 WHERE hotels.id NOT IN (SELECT hotels FROM offers);
