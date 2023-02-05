4447b26ba62613a9c06b4dcb5eec23475151cddd
xam1DAW-M <118368998+xam1DAW-M@users.noreply.github.com>
2023-02-05 15:04:50 +0100

    Update

--- Ejercicio6.sql
+++ Ejercicio6.sql
@@ -40,7 +40,7 @@ INSERT INTO places(id,name,area,container) VALUES (7,'Santa Pola',3,1);
 INSERT INTO places(id,name,area,container) VALUES (8,'Gran Alacant',1,7);
 INSERT INTO places(id,name,area,container) VALUES (9,'Jaen',60,10);
 INSERT INTO places(id,name,area) VALUES (10,'España',1000);
-
+SELECT * FROM places;
 CREATE TABLE has_nacionality
 (
   places BIGINT,
@@ -48,7 +48,30 @@ CREATE TABLE has_nacionality
   dni VARCHAR(64),
   CONSTRAINT pk_has_nacionality PRIMARY KEY(places,persons),
   CONSTRAINT uk_dni_hn UNIQUE (dni),
-  CONSTRAINT ch_dni_len CHECK (LEN(dni)<5),
+  CONSTRAINT ch_dni_len CHECK (LEN(dni)>5),
   CONSTRAINT fk_places_hn FOREIGN KEY (places) REFERENCES places(id) on UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT fk_persons_hn FOREIGN KEY (persons) REFERENCES persons(id) on UPDATE CASCADE ON DELETE CASCADE,
 );
+INSERT INTO has_nacionality(places,persons,dni) VALUES (1,2,'aaaaaa');
+INSERT INTO has_nacionality(places,persons,dni) VALUES (2,1,'aaahjab');
+INSERT INTO has_nacionality(places,persons,dni) VALUES (4,3,'aabaca');
+INSERT INTO has_nacionality(places,persons,dni) VALUES (3,4,'vvbaaa');
+INSERT INTO has_nacionality(places,persons,dni) VALUES (10,5,'aaayaaa');
+INSERT INTO has_nacionality(places,persons,dni) VALUES (7,6,'aaaaabb');
+INSERT INTO has_nacionality(places,persons,dni) VALUES (5,7,'aaaddaa');
+INSERT INTO has_nacionality(places,persons,dni) VALUES (6,8,'aacvaaa');
+INSERT INTO has_nacionality(places,persons,dni) VALUES (1,9,'aaaepaa');
+INSERT INTO has_nacionality(places,persons,dni) VALUES (4,10,'aaayaa');
+
+/*Ejercicio 1*/
+SELECT name, civil_status
+FROM persons;
+
+/*Ejercicio 2*/
+SELECT p.name, h.dni
+FROM persons as p LEFT JOIN has_nacionality as h on p.id = h.persons;
+
+/*Ejercicio 3*/
+SELECT p.name, COUNT(*)
+FROM persons AS p INNER JOIN has_nacionality as h ON p.id = h.persons
+GROUP BY p.name, h.dni;
