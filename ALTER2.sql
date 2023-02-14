f559e04d2c71e1fd06a56d6e377c5fa8a6947bd8
xam1DAW-M <118368998+xam1DAW-M@users.noreply.github.com>
2023-02-14 13:18:16 +0100

    Update ALTER2.sql

--- ALTER2.sql
+++ ALTER2.sql
@@ -2,14 +2,14 @@ CREATE TABLE ASIGNATURA
 (
   codigo INT,
   nombre VARCHAR(20),
-  CONSTRAINT pk_asignatura_codigo PRIMARY KEY(codigo)
+  CONSTRAINT pk_asignatura_codigo PRIMARY KEY(codigo),
 );
 
 CREATE TABLE DEPARTAMENTO
 (
   cod INT,
   nombre VARCHAR(20),
-  CONSTRAINT pk_departamento_cod PRIMARY KEY(cod)
+  CONSTRAINT pk_departamento_cod PRIMARY KEY(cod),
 );
 
 CREATE TABLE PROFESOR
@@ -20,7 +20,7 @@ CREATE TABLE PROFESOR
   edad INT, 
   departamento INT,
   CONSTRAINT pk_profesor_dni PRIMARY KEY(dni),
-  CONSTRAINT fk_departamento_profesor FOREIGN KEY(departamento) REFERENCES DEPARTAMENTO(cod) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_departamento_profesor FOREIGN KEY(departamento) REFERENCES DEPARTAMENTO(cod) ON UPDATE CASCADE ON DELETE CASCADE,
 );
 
 CREATE TABLE imparte 
@@ -29,7 +29,7 @@ CREATE TABLE imparte
   profesor VARCHAR(9),
   CONSTRAINT pk_asig_profe_imparte PRIMARY KEY(asignatura, profesor),
   CONSTRAINT fk_asig_imparte FOREIGN KEY (asignatura) REFERENCES ASIGNATURA(codigo) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT fk_profesor_imparte FOREIGN KEY (profesor) REFERENCES PROFESOR(dni) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_profesor_imparte FOREIGN KEY (profesor) REFERENCES PROFESOR(dni) ON UPDATE CASCADE ON DELETE CASCADE,
 );
 
 CREATE TABLE CODIFICACION
@@ -37,8 +37,8 @@ CREATE TABLE CODIFICACION
   id INT,
   tipo VARCHAR(20),
   video INT,
-  CONSTRAINT pk_codificacion PRIMARY KEY (id,video)
-  CONSTRAINT fk_video FOREIGN KEY (video) REFERENCES VIDEO(id);
+  CONSTRAINT pk_codificacion PRIMARY KEY (id,video),
+  CONSTRAINT fk_video FOREIGN KEY (video) REFERENCES VIDEO(id)
   
 
 );
@@ -59,7 +59,7 @@ CREATE TABLE es_de
   video INT,
   CONSTRAINT pk_es_de PRIMARY KEY (profesor, video),
   CONSTRAINT fk_profesor_es_de FOREIGN KEY (profesor) REFERENCES PROFESOR(dni) ON UPDATE CASCADE ON DELETE CASCADE,
-  CONSTRAINT fk_video_es_de FOREIGN KEY (video) REFERENCES VIDEO(id) ON UPDATE CASCADE ON DELETE CASCADE
+  CONSTRAINT fk_video_es_de FOREIGN KEY (video) REFERENCES VIDEO(id) ON UPDATE CASCADE ON DELETE CASCADE,
 );
 
 -- Introduce los siguientes valores. Si hay error en algún insert, explica en cual y justifica el porqué.
@@ -78,7 +78,7 @@ INSERT INTO PROFESOR (dni, nombre, apellidos, edad, departamento) VALUES ('00000
 INSERT INTO PROFESOR (dni, nombre, apellidos, edad, departamento) VALUES ('11111111B', 'MERCEDES', 'LÓPEZ MARTÍ', 37, 0);
 INSERT INTO PROFESOR (dni, nombre, apellidos, edad, departamento) VALUES ('22222222C', 'JULIO', 'JORNET VIVES', 37, 1)
 INSERT INTO PROFESOR (dni, nombre, apellidos, edad) VALUES ('33333333D', 'AMPARO', 'ESTEVE BLASCO', 33);
-INSERT INTO PROFESOR (dni, nombre, apellidos, edad, departamento VALUES ('33333333D', 'AMPARO', 'ESTEVE BLASCO', 33,5); --Mal, ya que repite la PK
+INSERT INTO PROFESOR (dni, nombre, apellidos, edad, departamento) VALUES ('33333333D', 'AMPARO', 'ESTEVE BLASCO', 33,5); --Mal, ya que repite la PK
 
 
 INSERT INTO VIDEO (id, titulo, descripcion) VALUES (0, 'VIDEO 0', 'DESCRIPCIÓN 0');
@@ -132,5 +132,62 @@ ADD CONSTRAINT fk_dep_asig FOREIGN KEY (asignatura) REFERENCES ASIGNATURA(codigo
                                        
 SELECT * FROM DEPARTAMENTO; --NULL
                       
-INSERT INTO DEPARTAMENTO (cod, nombre, asignatura) VALUES (4,'iNFORMATICA',1)   
-                  
+INSERT INTO DEPARTAMENTO (cod, nombre, asignatura) VALUES (4,'iNFORMATICA',1);
+                      
+ALTER TABLE VIDEO
+ADD CONSTRAINT df_video_descripcion DEFAULT 'El video no tiene descripción' FOR descripcion;
+
+INSERT INTO VIDEO (id, titulo, descripcion) VALUES (3,'VIDEO 3', NULL);
+
+INSERT INTO VIDEO (id, titulo) VALUES (4,'VIDEO 4'); --No se puede, ya que descripción es un varchar 20
+
+ALTER TABLE VIDEO
+ALTER COLUMN descripcion VARCHAR(40); --Con esto ya funcionaría
+
+ALTER TABLE PROFESOR
+ALTER COLUMN edad BIGINT; --No se altera ya que hay un check que depende de esa columna
+
+ALTER TABLE PROFESOR
+DROP CONSTRAINT ck_edad_profesor; --Ahora ya podemos.
+
+ALTER TABLE PROFESOR
+ADD CONSTRAINT ck_edad_profesor CHECK(edad  BETWEEN 16 AND 75);   --Volvemos a hacer el check.
+
+ALTER TABLE ASIGNATURA
+ALTER COLUMN codigo BIGINT; --No se puede ya que es el pk.
+
+ALTER TABLE ASIGNATURA
+DROP CONSTRAINT pk_asignatura_codigo;  --No se puede porque esta de fk en Departamento.
+
+ALTER TABLE DEPARTAMENTO
+DROP CONSTRAINT fk_dep_asig; --Sigue sin poderse ya que está tambien en imparte.
+
+ALTER TABLE imparte
+DROP CONSTRAINT fk_asig_imparte; --ahora si podemos eliminar la pk y hacerlo BIGINT
+
+ALTER TABLE ASIGNATURA
+ALTER COLUMN codigo BIGINT NOT NULL; --NN ya que si no es not null no se puede hacer pk
+
+ALTER TABLE ASIGNATURA
+ADD CONSTRAINT pk_asignatura_codigo PRIMARY KEY(codigo);
+
+ALTER TABLE DEPARTAMENTO
+ADD CONSTRAINT fk_dep_asig FOREIGN KEY (asignatura) REFERENCES ASIGNATURA(codigo); --No se puede ya que no tiene el mismo data type
+
+ALTER TABLE DEPARTAMENTO
+ALTER COLUMN asignatura BIGINT; --Ahora si se puede
+
+ALTER TABLE imparte
+ALTER COLUMN asignatura BIGINT; --No se puede es pk.
+
+ALTER TABLE imparte
+DROP CONSTRAINT pk_asig_profe_imparte;
+
+ALTER TABLE imparte
+ALTER COLUMN asignatura BIGINT NOT NULL;
+
+ALTER TABLE imparte
+ADD CONSTRAINT pk_asig_profe_imparte PRIMARY KEY(asignatura, profesor);
+
+ALTER TABLE imparte
+ADD CONSTRAINT fk_asig_imparte FOREIGN KEY (asignatura) REFERENCES ASIGNATURA(codigo) ON UPDATE CASCADE ON DELETE CASCADE;
